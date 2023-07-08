24b2c48422093fde7195881075b2770a639de6c7
Bidhan Rakshit <90621019+Bidhan77@users.noreply.github.com>
2023-07-08 14:35:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function decimalToBinary(num) {
+function decimalToBinary(decimal) {
   //Write you code here
+	let binary = "";
+
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+
+  return binary;
   
 }
 
